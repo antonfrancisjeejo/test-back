50b52393bf2f1fe17e05beb7e020122aef5af07d
Anton Francis Jeejo <antonfrancisjeejo13@gmail.com>
2022-12-06 01:25:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("It is working");
 });
 
+app.get("/:name", (req, res) => {
+  res.send("Directly from node server "+req.params.name);
+});
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
